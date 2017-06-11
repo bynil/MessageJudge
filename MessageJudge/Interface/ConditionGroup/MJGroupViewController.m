84a453a7306d79bf1a0b529fc10c1f68280af973
gexiao <gexiao1994@gmail.com>
2017-06-12 00:00:14 +0800

    Group group view controller files

--- MessageJudge/Interface/ConditionGroup/MJGroupViewController.m
+++ MessageJudge/Interface/ConditionGroup/MJGroupViewController.m
@@ -28,13 +28,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNewCondition)];
     [self.tableView registerClass:MJConditionCell.class forCellReuseIdentifier:MJConditionCellReuseIdentifier];
+}
+
+#pragma mark - Action
+- (void)addNewCondition {
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 #pragma mark - Table view data source
